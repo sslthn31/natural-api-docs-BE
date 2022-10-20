c8de9a82a8336a55ecc7f2c386b1edb38fe227ba
sslthn31 <alhambra.sulthan@gmail.com>
2022-10-20 23:15:52 +0700

    more function

--- controller/index.js
+++ controller/index.js
@@ -71,3 +71,61 @@ res.status(200).json({
   console.log(error)  
 }
 }
+
+export const hammingStringDistance = async(req, res) => {
+    try {
+    const paramOne = await req.params.paramFirst;
+    const paramTwo = await req.params.paramSecond;
+    const hamming = natural.HammingDistance(paramOne, paramTwo, false);
+    res.status(200).json({
+        message: "Success",
+        data: hamming
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const jaroWinklerStringDistance = async(req, res) => {
+    try {
+    const paramOne = await req.params.paramFirst;
+    const paramTwo = await req.params.paramSecond;
+    const jaroWinkler = natural.JaroWinklerDistance(paramOne, paramTwo);
+    res.status(200).json({
+        message: "Success",
+        data: jaroWinkler
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const levenshteinStringDistance = async(req, res) => {
+    try {
+    const paramOne = await req.params.paramFirst;
+    const paramTwo = await req.params.paramTwo;
+    const levenshetein = natural.LevenshteinDistance(paramOne, paramTwo);
+
+    res.status(200).json({
+        message: 'Success',
+        data: levenshetein
+    })
+    } catch (error) {
+    console.log(error)    
+    }
+}
+
+export const diceCoefficientStringDistance = async(req, res) => {
+    try {
+        const paramOne = await req.params.paramFirst;
+        const paramTwo = await req.params.paramSecond;
+        const diceCoefficient = natural.DiceCoefficient(paramOne, paramTwo);
+
+        res.status(200).json({
+            message: "Success",
+            data: diceCoefficient
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { welcome, Metaphone, Tokenizer } from "../controller/index.js";
+import { welcome, Metaphone, Tokenizer, nounInflectorsPlural, nounInflectorSingular, hammingStringDistance, jaroWinklerStringDistance, levenshteinStringDistance, diceCoefficientStringDistance } from "../controller/index.js";
 
 const routes = express.Router();
 
@@ -9,5 +9,11 @@ const routes = express.Router();
 routes.get('/', welcome)
 routes.get('/naturalApi/phonetics/:paramFirst&:paramSecond', Metaphone)
 routes.get('/naturalApi/tokenizer/:paramFirst', Tokenizer)
+routes.get('/naturalApi/plural/:paramFirst', nounInflectorsPlural)
+routes.get('/naturalApi/singular/:paramFirst', nounInflectorSingular)
+routes.get('/naturalApi/hamming/:paramFirst&:paramSecond', hammingStringDistance)
+routes.get('/naturalApi/jarowinkler/:paramFirst&:paramSecond', jaroWinklerStringDistance)
+routes.get('/naturalApi/levenshtein/:paramFirst&:paramSecond', levenshteinStringDistance)
+routes.get('/naturalApi/dicecoefficient/:paramFirst&:paramSecond', diceCoefficientStringDistance)
 
 export default routes;
