ed08b6daf8890f554dcf576d2351cd93af0123cc
sslthn31 <alhambra.sulthan@gmail.com>
2022-12-06 09:43:22 +0700

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import routes from './routes/index.js';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(cors())
 // app.get('/', (req, res) => {
